5504123c5af8cf4d35336cdca31db60d5afe62f2
Felix2048 <infinitesamsarax@outlook.com>
2019-12-30 18:13:15 +0800

    Add Transmission

---
+++ dehazer/core/Transmission.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from dehazer.core.DarkChannel import getDarkChannel
+
+
+def getTransmission(I, darkChannel, A, w=0.95, patchSize=15):
+    """
+    Get the transmission t of the RGB image data from a numpy array
+
+    # Arguments
+
+    - I:    3 * M * N numpy array of the input image, where 3 stands for
+          the RGB channels, M is the height, and N is the width
+
+    - darkChannel:  the dark channel prior of the image as an M * N numpy array
+
+    - A:    3-element list contains atmosphere light ([0, L-1]) for each RGB
+            channel
+
+    - w:    a constant parameter (0 < w <= 1) to optionally keep a very small
+            amount of haze for the distant objects (aerial perspective)
+
+    - patchSize:  patch size
+
+    # Returns
+
+    - M * N numpy array of the transmission rate [0.0, 1.0] of the input image
+
+    """
+
+    t = 1 - w * getDarkChannel(I / A, patchSize)
+
+    return t
--- dehazer/test/CoreTest.py
+++ dehazer/test/CoreTest.py
@@ -13,30 +13,41 @@ class CoreTest(unittest.TestCase):
         self.origin = Image.open('../../image/forest1.jpg')
         self.image = np.asarray(self.origin)
         self.darkChannel = DarkChannel.getDarkChannel(self.image)
+        self.A = Atmosphere.getAtmosphere(self.image, self.darkChannel)
+        self.t = Transmission.getTransmission(
+            self.image, self.darkChannel, self.A)
 
     # def test_dark_channel(self):
-    #     plt.figure('origin')
+    #     plt.figure('OriginImage')
     #     plt.imshow(self.origin)
     #     plt.show()
-    #     self.darkChannel = DarkChannel.getDarkChannel(self.image)
-    #     plt.figure('darkChannel')
-    #     plt.imshow(self.darkChannel, cmap='gray')
+    #     darkChannel = DarkChannel.getDarkChannel(self.image)
+    #     plt.figure('DarkChannel')
+    #     plt.imshow(darkChannel, cmap='gray')
     #     plt.show()
-    #     self.assertTrue(self.darkChannel is not None)
+    #     self.assertTrue(darkChannel is not None)
 
-    def test_atmosphere(self):
-        p = 0.0001
-        maxA = 220
-        A = Atmosphere.getAtmosphere(
-            self.image, self.darkChannel, p=p, maxA=maxA)
-        self.assertIsNotNone(A)
-        self.assertLessEqual(A[0], maxA)
-        self.assertLessEqual(A[1], maxA)
-        self.assertLessEqual(A[2], maxA)
+    # def test_atmosphere(self):
+    #     p = 0.0001
+    #     maxA = 220
+    #     A = Atmosphere.getAtmosphere(
+    #         self.image, self.darkChannel, p=p, maxA=maxA)
+    #     self.assertIsNotNone(A)
+    #     self.assertLessEqual(A[0], maxA)
+    #     self.assertLessEqual(A[1], maxA)
+    #     self.assertLessEqual(A[2], maxA)
 
-        colors = ['r', 'g', 'b']
-        plt.title('Atmosphere')
-        plt.scatter([0, 1, 2], A, c=colors, marker='o')
-        plt.xlabel('RGB Channel')
-        plt.ylabel('Atmosphere Value')
+    #     colors = ['r', 'g', 'b']
+    #     plt.title('Atmosphere')
+    #     plt.scatter([0, 1, 2], A, c=colors, marker='o')
+    #     plt.xlabel('RGB Channel')
+    #     plt.ylabel('Atmosphere Value')
+    #     plt.show()
+
+    def test_transmission(self):
+        t = Transmission.getTransmission(
+            self.image, self.darkChannel, self.A)
+        plt.figure('Transmission')
+        plt.imshow(t)
         plt.show()
+        self.assertTrue(t is not None)
